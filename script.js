a7f5afd156ed398bd021caf8596b87742cd515e5
Andrei <apatskou@gmail.com>
2023-04-13 17:54:54 +0300

    some new function added

--- script.js
+++ script.js
@@ -24,14 +24,14 @@ function intNumbers(a, b){
     return (!Number.isInteger(a) || !Number.isInteger(b)) ? 'Error!' : (b - a == 1) ? '' : (a + 1) + ' ' + intNumbers(a + 1, b);
 }
 
-//function rerurns Fibonacci numbers
+//function returns Fibonacci numbers
 function FibonacciNumber(number) {
     return (!Number.isInteger(number) || number < 0) ? 'Error!' : (number <= 1) ? number : (FibonacciNumber(number - 1) + FibonacciNumber(number - 2));
 }
 
 console.log(FibonacciNumber(-6));
 
-//function rerurns Fibonacci numbers
+//function returns Fibonacci numbers
 function FibonacciNumber(number) {
     return (!Number.isInteger(number) || number < 0) ? 'Error!' : (number <= 1) ? number : (FibonacciNumber(number - 1) + FibonacciNumber(number - 2));
 }
@@ -43,7 +43,49 @@ function pow(x, n) {
     return (!Number.isInteger(x) || !Number.isInteger(n)) ? 'Error!' : (n === 1) ? x : x * pow(x, n - 1);
 }
 
-//рекурсия НОД
-function NOD(a, b) {
-    return ( ! b) ? a : NOD(b, a % b);
+//Function returns the greatest common factor 
+function GCF(a, b) {
+    return ( ! b) ? a : GCF(b, a % b);
+}
+
+//Function checks if all brackets are in pairs
+function checkBrackets(str){
+    let stack = [];
+    let brackets = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+
+    for (let char of str){
+        if (['(', '[', '{'].includes(char)){
+            stack.push(char);
+        }else{
+            let lastChar = stack.pop();
+            if (char !== brackets[lastChar]){
+                return false;
+            }
+        }
+    }
+    return (stack.length === 0) ? true : false;
+}
+
+//Function returns array without all repeatable elements
+function withoutRepeatElements(arr){
+    const result = [];
+    const obj = {};
+    
+    for (let i = 0; i < arr.length; i++){
+        const currElem = arr[i];
+        (currElem in obj) ? obj[currElem] += 1 : obj[currElem] = 1;
+    }
+
+    const keys = Object.keys(obj);
+    keys.forEach((key) => {
+        if (obj[key] === 1){
+            result.push(key);
+        }
+    });
+
+    return result;
 }
